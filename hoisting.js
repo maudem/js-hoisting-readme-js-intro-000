639bf44210440fc8ae802a7b6de3f7d7df6d1a17
maude sos <mualth@gmail.com>
2017-09-12 01:41:02 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -19,11 +19,10 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
+  
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
